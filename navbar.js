0d12e10824dd6bee4dcd6508e1b6ac76c744a073
Jorge-duoc <jo.ruizm@duocuc.cl>
2025-09-07 21:35:34 -0300

    commit

--- navbar.js
+++ navbar.js
@@ -18,13 +18,12 @@ document.addEventListener("DOMContentLoaded",() => {
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Categorias
+              Productos
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="Instrumentos.html">Instrumentos</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="Albumes.html">Álbumes</a></li>
             </ul>
           </li>
 
